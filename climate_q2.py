51f315ffc6d1255ddc73f335cf41ad32475bbeaf
Kaichunyu <kaichun.yu@griffithuni.edu.au>
2023-09-13 16:26:37 +1000

    updated climate_q2.py and result figure

--- climate_q2.py
+++ climate_q2.py
@@ -5,6 +5,25 @@ years = []
 co2 = []
 temp = []
 
+df = pd.read_csv(r"climate.csv")
+
+year_data = df['Year']
+co2_data = df['CO2']
+temp_data = df['Temperature']
+
+for i in year_data:
+    years.append(i)
+print(years)
+
+for i in co2_data:
+    co2.append(i)
+print(co2)
+
+for i in temp_data:
+    temp.append(i)
+print(temp)
+
+
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
